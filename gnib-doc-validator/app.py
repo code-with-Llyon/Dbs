97a412d910e30a5ee2aa89e172f055aff670554c
Eze Ubachukwu <145666988+code-with-Llyon@users.noreply.github.com>
2025-12-05 11:43:34 +0000

    ref: https://flask.palletsprojects.com/en/stable/patterns/fileuploads/

--- gnib-doc-validator/app.py
+++ gnib-doc-validator/app.py
@@ -82,6 +82,25 @@ def index():
     ensure_session_store()
     return render_template("index.html")
 
+#routing the upload document  
+#using template
 
-
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('download_file', name=filename))
+    return '''
 
